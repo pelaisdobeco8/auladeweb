d5c6fd3788a79960b479008ff3b4b596cc411981
Beatriz <Beatriz@DESKTOP-3RCJ6CF>
2019-02-18 21:28:43 -0300

    ex4

---
+++ ex4.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Aula Web</title>
+    </head>
+    <body>
+        <?php
+            for ($i=1; $i<100; $i++){
+               if ($i%2 == 0) {
+                   echo $i."\n";
+               }
+           } 
+        ?>
+    </body>
+</html>
