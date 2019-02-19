b2742f8800b7828413a2e84f32be8034f07d3501
Beatriz <Beatriz@DESKTOP-3RCJ6CF>
2019-02-18 21:32:21 -0300

    ex5

---
+++ ex5.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Aula Web</title>
+    </head>
+    <body>
+        <?php
+            $n1 = 30;
+            $n2 = 20;
+            
+            if ($n1>$n2) {
+                echo "O maior valor é: ". $n1;
+            }else {
+                echo "O maior valor é: ". $n2;
+            }
+        ?>
+    </body>
+</html>
